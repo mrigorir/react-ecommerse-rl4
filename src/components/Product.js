65c5bc7b65e27ab8b94251576a515f0196726516
Marco Parra <marcoparra311@gmail.com>
2022-01-11 14:11:30 -0400

    Create & style product card

---
+++ src/components/Product.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { AddShoppingCart } from '@mui/icons-material';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Product() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const addToBasket = () => {
+    console.log('added');
+  }
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        action={
+          <Typography variant="h5" color="text.secondary" aria-label="price">
+            $50
+          </Typography>
+        }
+        title="Shoes"
+        subheader="In stock"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://s2.r29static.com/bin/entry/ebd/0,675,2000,1050/x,80/1929471/image.jpg"
+        alt="Nike shoes"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Running shoes
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to cart" onClick={addToBasket}>
+          <AddShoppingCart fontSize="large"/>
+        </IconButton>
+        {Array(4)
+        .fill('someEmptyValue')
+        .map((_, i) => (
+          <p key={i + 1}>
+            &#11088;
+          </p>
+        ))}
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Nice and attractive nike shoes.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
