8a9089e78312c7acea3722009557385156dd4a3f
Marco Parra <marcoparra311@gmail.com>
2022-01-13 15:37:02 -0400

    Create NavBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Product from './components/Product';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <h1>
-        Some title...
-      </h1>
+      <NavBar />
       <Product />
     </>
   );
